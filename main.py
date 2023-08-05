0d861515ef2ba82089ac1ea8e0cc5b1dfeba5260
arpita837 <71542464+arpita837@users.noreply.github.com>
2023-08-05 20:01:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,100 +1,95 @@
+class Contact:
+    def __init__(self, name, phone, email):
+        self.name = name
+        self.phone = phone
+        self.email = email
+
 class ContactBook:
     def __init__(self):
-        self.contacts = {}
+        self.contacts = []
 
-    def add_contact(self, name, details):
-        self.contacts[name] = details
-        print(f"Contact '{name}' added successfully!")
+    def add_contact(self, contact):
+        self.contacts.append(contact)
 
     def search_contact(self, name):
-        if name in self.contacts:
-            return self.contacts[name]
-        else:
-            return None
+        for contact in self.contacts:
+            if contact.name.lower() == name.lower():
+                return contact
+        return None
 
-    def update_contact(self, name, details):
-        if name in self.contacts:
-            self.contacts[name] = details
-            print(f"Contact '{name}' updated successfully!")
+    def update_contact(self, name, phone, email):
+        contact = self.search_contact(name)
+        if contact:
+            contact.phone = phone
+            contact.email = email
+            print(f"Contact '{name}' updated.")
         else:
-            print(f"Contact '{name}' not found!")
+            print(f"Contact '{name}' not found.")
 
     def delete_contact(self, name):
-        if name in self.contacts:
-            del self.contacts[name]
-            print(f"Contact '{name}' deleted successfully!")
+        contact = self.search_contact(name)
+        if contact:
+            self.contacts.remove(contact)
+            print(f"Contact '{name}' deleted.")
         else:
-            print(f"Contact '{name}' not found!")
+            print(f"Contact '{name}' not found.")
 
-    def display_contact_book(self):
-        if self.contacts:
-            print("Contact Book:")
-            for name, details in self.contacts.items():
-                print(f"{name}: {details}")
-        else:
-            print("Contact book is empty!")
+    def display_contacts(self):
+        print("Contact Book:")
+        for contact in self.contacts:
+            print(f"Name: {contact.name}\nPhone: {contact.phone}\nEmail: {contact.email}\n")
 
-    def display_contacts_with_alphabet(self, alphabet):
-        matching_contacts = {name: details for name, details in self.contacts.items() if name.startswith(alphabet)}
-        if matching_contacts:
-            print(f"Contacts with the alphabet '{alphabet}':")
-            for name, details in matching_contacts.items():
-                print(f"{name}: {details}")
-        else:
-            print(f"No contacts found with the alphabet '{alphabet}'.")
-
+    def display_by_alphabet(self, alphabet):
+        print(f"Contacts starting with '{alphabet}':")
+        for contact in self.contacts:
+            if contact.name.lower().startswith(alphabet.lower()):
+                print(f"Name: {contact.name}\nPhone: {contact.phone}\nEmail: {contact.email}\n")
 
 def main():
     contact_book = ContactBook()
 
     while True:
-        print("\n--- Contact Book Menu ---")
-        print("1. Add new contact")
-        print("2. Search for a person")
-        print("3. Update existing contact")
-        print("4. Delete existing contact")
-        print("5. Display all contacts")
-        print("6. Display contacts with specific alphabet")
+        print("\n1. Add Contact")
+        print("2. Search Contact")
+        print("3. Update Contact")
+        print("4. Delete Contact")
+        print("5. Display All Contacts")
+        print("6. Display Contacts by Alphabet")
         print("7. Exit")
 
-        choice = int(input("Enter your choice (1-7): "))
+        choice = int(input("Select an option: "))
 
         if choice == 1:
-            name = input("Enter the name: ")
-            details = input("Enter the details: ")
-            contact_book.add_contact(name, details)
-
+            name = input("Name: ")
+            phone = input("Phone: ")
+            email = input("Email: ")
+            contact = Contact(name, phone, email)
+            contact_book.add_contact(contact)
+            print("Contact added.")
         elif choice == 2:
-            name = input("Enter the name to search: ")
-            result = contact_book.search_contact(name)
-            if result:
-                print(f"Contact Details:\n{name}: {result}")
+            name = input("Enter name to search: ")
+            contact = contact_book.search_contact(name)
+            if contact:
+                print(f"Name: {contact.name}\nPhone: {contact.phone}\nEmail: {contact.email}\n")
             else:
-                print(f"Contact '{name}' not found!")
-
+                print(f"Contact '{name}' not found.")
         elif choice == 3:
-            name = input("Enter the name to update: ")
-            details = input("Enter the new details: ")
-            contact_book.update_contact(name, details)
-
+            name = input("Enter name to update: ")
+            phone = input("New phone: ")
+            email = input("New email: ")
+            contact_book.update_contact(name, phone, email)
         elif choice == 4:
-            name = input("Enter the name to delete: ")
+            name = input("Enter name to delete: ")
             contact_book.delete_contact(name)
-
         elif choice == 5:
-            contact_book.display_contact_book()
-
+            contact_book.display_contacts()
         elif choice == 6:
-            alphabet = input("Enter the alphabet: ")
-            contact_book.display_contacts_with_alphabet(alphabet)
-
+            alphabet = input("Enter an alphabet: ")
+            contact_book.display_by_alphabet(alphabet)
         elif choice == 7:
-            print("Exiting the Contact Book.")
             break
-
         else:
-            print("Invalid choice. Please choose again.")
-
+            print("Invalid choice. Please select again.")
 
 if __name__ == "__main__":
     main()
